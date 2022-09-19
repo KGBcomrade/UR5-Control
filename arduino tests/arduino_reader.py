8e752faa6de8b300cccaa90bcfefa5930b95461f
Hacker1337 <amirfvb@gmail.com>
2022-09-19 19:06:47 +0300

    Arduinon read example

---
+++ arduino tests/arduino_reader.py
@@ -0,0 +1,21 @@
+# Importing Libraries
+import serial
+import time
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+def write_read(x):
+    # arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.15)
+    data = arduino.readline()
+    return data
+while True:
+    # num = input("Enter a number: ") # Taking input from user
+    # value = write_read(num)
+    
+    # value = write_read(10)
+    value = arduino.readline()
+    time.sleep(1)
+
+    # value = arduino.read()
+    print(value) # printing the value
+    print(float(value))
+
