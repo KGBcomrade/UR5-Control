afa47e82911c49a74574ce2893bf29c7bba80aa7
Hacker1337 <amirfvb@gmail.com>
2022-09-26 12:24:39 +0300

    Rewrite grid toucher from force to coordinatte

--- grid_toucher.py
+++ grid_toucher.py
@@ -1,6 +1,6 @@
-from cmath import nan
 import numpy as np
 import yaml
+import time
 
 ## sacred staff
 import os
@@ -26,14 +26,6 @@ else:
     ex.observers.append(FileStorageObserver('logdir'))
     input("WARNING! No password for db. Confirm logging locally")
 ex.add_config('params.yaml')
-
-# import glob
-# for source_file in glob.glob("**/*.py", recursive=True):
-#     ex.add_source_file(source_file)
-    
-# @ex.capture
-# def log(_run):
-    # _run.log_scalar('', 12)
 
 with open('params.yaml') as conf_file:
     config = yaml.safe_load(conf_file)
@@ -72,9 +64,6 @@ def touch_sensor(_run):
     print("Shape is", shape)
     net_save_hight = config['safe_hight']
 
-    # shape = [10, 10]
-    
-    # shape = [10, 10]
     net_corner = p0
     net_massive = [
         [ [net_corner[0]+i*net_step[0], net_corner[1]+j*net_step[1]]
@@ -89,12 +78,9 @@ def touch_sensor(_run):
             print(point)
             point_results = {"target_coordinate": point,
                              "base_coordinate":[],
-                             "force_z":[],
                              "vector_force":[],
                              "deformation":[],
                              "base_coordinate":[],
-                             'power': [],
-                             'aim_force': [],
                              'tenso_signal': [],
                              'final_power': [],
                              }
@@ -106,63 +92,35 @@ def touch_sensor(_run):
             rtde_c.moveL(c_state[:2] + [net_save_hight] + c_state[3:6], *config['speed'])
             rtde_c.moveL(point + [net_save_hight] + c_state[3:6], *config['speed'])
             
-            dt = 1.0/config['working_freq']
-            push_time_list = config['push_time']
-            force_list = config['force']
+            target_depthes = np.linspace(config['sensor_hight'], 
+                                         config['sensor_hight'] - config['max_sensor_depth'],
+                                         config['sensor_depth_points'], 
+                                         endpoint=True)
             
-            task_frame = [0, 0, 0, 0, 0, 0]
-            selection_vector = [0, 0, 1, 0, 0, 0]
-            if type(force_list) != type([]):
-                force_list = [force_list]
-                push_time_list = [push_time_list]
-            for force, push_time in zip(force_list, push_time_list):
-                rtde_c.forceModeStop()
-                wrench = [0, 0, -force, 0, 0, 0]   # forced in Newtons
-                force_type = 2
-                limits = [2, 2, 1.5, 1, 1, 1]   # limits speeds in directions of main movement and deviations in others
+            for depth in target_depthes:
+                ## moving
+                rtde_c.moveL(point + [depth] + c_state[3:6], *config['speed'])
+                time.sleep(config['wait_time'])
+                ## logging
+                point_results['base_coordinate'].append(rtde_r.getActualTCPPose())
+                point_results['vector_force'].append(rtde_r.getActualTCPForce())
 
-                # Execute 500Hz control loop for 4 seconds, each cycle is 2ms
-                for i in range(int(push_time/dt)):
-                    rtde_c.initPeriod()
-                    # First move the robot down for 2 seconds, then up for 2 seconds
-                    rtde_c.forceMode(task_frame, selection_vector, wrench, force_type, limits)
-    
-                    ## logging
-                    point_results['base_coordinate'].append(rtde_r.getActualTCPPose())
-                    _run.log_scalar('force_z', rtde_r.getActualTCPForce()[2])
-                    point_results['force_z'].append(rtde_r.getActualTCPForce()[2])
-                    point_results['vector_force'].append(rtde_r.getActualTCPForce())
-                    depth = max(0, config['sensor_hight']-rtde_r.getActualTCPPose()[2])
-                    # _run.log_scalar('deformation', depth)
-                    point_results['deformation'].append(depth)
-
-                    power = float(rsrc.query('measure:power?'))
-                    point_results['power'].append(power)
-                    _run.log_scalar('power', power)
-                    
-                    # if depth > config['max_sensor_depth']:
-                    #     print("max depth stop")
-                    #     break
-                    rtde_c.waitPeriod(dt)
-                # else:
-                #     print("time limit stop")
-                
-                ## log something after maximal deformation is reached
-                point_results['aim_force'].append(force)
-                
+                point_results['deformation'].append(depth)
+                _run.log_scalar('deformation', depth)
+                                    
                 tenso_string = arduino.readline()
                 if (tenso_string == b'' or tenso_string == ''):
-                    tenso_value = nan
+                    tenso_value = None
                 else:
                     tenso_value = float(tenso_string)
                 point_results['tenso_signal'].append(tenso_value)
                 
-                # point_results['tenso_signal'].append(tenso_string)
                 
-                point_results['final_power'].append(float(rsrc.query('measure:power?')))
+                power = float(rsrc.query('measure:power?'))
+                _run.log_scalar('power', power)
+                point_results['final_power'].append(power)
                 
                 
-            rtde_c.forceModeStop()
             _run.log_scalar('point_results', point_results)
             rtde_c.moveL(point + [net_save_hight] + c_state[3:6], *config['speed'])
             
